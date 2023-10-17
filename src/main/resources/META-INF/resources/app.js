a0dd953ee5b41409cdeba496b3beab9b727b6552
zwarenelle <tomie121@gmail.com>
2023-10-17 14:50:23 +0200

    Rename Job Skills

--- src/main/resources/META-INF/resources/app.js
+++ src/main/resources/META-INF/resources/app.js
@@ -130,12 +130,12 @@ function refreshSchedule() {
                     .append($(`<p class="card-text ms-2 mb-0"/>`).text(`Due: ${job.dueDate}`));
                 const byJobJobElement = $(`<div/>`)
                   .append($(`<h5 class="card-title mb-1"/>`).text(`Unassigned`));
-                $.each(job.requiredSkills, (index, tag) => {
-                    if (tag.toString() == "Gas")
+                $.each(job.requiredSkills, (index) => {
+                    if (job.requiredSkills.bestekcode.toString() == "Gas")
                     {
                         color = "#FEB900";
                     }
-                    else if (tag.toString() == "Elektra")
+                    else if (job.requiredSkills.bestekcode.toString() == "Elektra")
                     {
                         color = "#ED5353";
                     }
@@ -143,8 +143,8 @@ function refreshSchedule() {
                     {
                         color = "#003366";
                     }
-                    unassignedJobElement.append($(`<span class="badge me-1" style="background-color: ${color}"/>`).text(tag));
-                    byJobJobElement.append($(`<span class="badge me-1" style="background-color: ${color}"/>`).text(tag));
+                    unassignedJobElement.append($(`<span class="badge me-1" style="background-color: ${color}"/>`).text(job.requiredSkills.bestekcode));
+                    byJobJobElement.append($(`<span class="badge me-1" style="background-color: ${color}"/>`).text(job.requiredSkills.bestekcode));
                 });
                 unassignedJobs.append($(`<div class="col"/>`).append($(`<div class="card"/>`).append(unassignedJobElement)));
                 byJobItemDataSet.add({
@@ -169,12 +169,12 @@ function refreshSchedule() {
                     byCrewJobElement.append($(`<p class="badge badge-danger mb-0"/>`).text(`After due (too late)`));
                     byJobJobElement.append($(`<p class="badge badge-danger mb-0"/>`).text(`After due (too late)`));
                 }
-                $.each(job.requiredSkills, (index, tag) => {
-                    if (tag.toString() == "Gas")
+                $.each(job.requiredSkills, (index) => {
+                    if (job.requiredSkills.bestekcode.toString() == "Gas")
                     {
                         color = "#FEB900";
                     }
-                    else if (tag.toString() == "Elektra")
+                    else if (job.requiredSkills.bestekcode.toString() == "Elektra")
                     {
                         color = "#ED5353";
                     }
@@ -182,8 +182,8 @@ function refreshSchedule() {
                     {
                         color = "#003366";
                     }
-                    byCrewJobElement.append($(`<span class="badge me-1" style="background-color: ${color}"/>`).text(tag));
-                    byJobJobElement.append($(`<span class="badge me-1" style="background-color: ${color}"/>`).text(tag));
+                    byCrewJobElement.append($(`<span class="badge me-1" style="background-color: ${color}"/>`).text(job.requiredSkills.bestekcode));
+                    byJobJobElement.append($(`<span class="badge me-1" style="background-color: ${color}"/>`).text(job.requiredSkills.bestekcode));
                 });
                 byCrewItemDataSet.add({
                     id : job.id, group: job.crew.id,
