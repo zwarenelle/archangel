78683a425b17475faad9512b1e67d9663869a8c0
Tom S <tomie121@gmail.com>
2023-12-15 11:58:00 +0100

    Add cluster

---
+++ src/main/java/nl/heijmans/teamplanning/domain/Cluster.java
@@ -0,0 +1,32 @@
+package nl.heijmans.teamplanning.domain;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Cluster {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL, orphanRemoval=true)
+    @JoinColumn(name="JOB_ID")
+    private List<Opdracht> opdrachten;
+
+    public List<Opdracht> getOpdrachten() {
+        return this.opdrachten;
+    }
+
+    public void setOpdrachten(List<Opdracht> opdrachten) {
+        this.opdrachten = opdrachten;
+    }
+    
+}
